bbaff4d31933f53c635dd91021df8c6c479b4f0f
PeterPark1221 <137192346+PeterPark1221@users.noreply.github.com>
2024-01-14 15:04:57 +0900

    Initial Commit

---
+++ Netflix/NetflixApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetflixApp.swift
+//  Netflix
+//
+//  Created by PeterPark on 1/14/24.
+//
+
+import SwiftUI
+
+@main
+struct NetflixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
