1afc7e621bf9fd01047cdb0b1302c8859380e164
PeterPark1221 <137192346+PeterPark1221@users.noreply.github.com>
2024-02-21 17:50:04 +0900

    modify TopRowButton and add Genre with Movie

---
+++ Netflix/HomeViews/HomeStack.swift
@@ -0,0 +1,18 @@
+//
+//  HomeStack.swift
+//  Netflix
+//
+//  Created by PeterPark on 2/21/24.
+//
+
+import SwiftUI
+
+struct HomeStack: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeStack()
+}
