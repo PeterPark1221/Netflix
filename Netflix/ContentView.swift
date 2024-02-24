d7c39854224044c28e9fdf0280aeeb86aa08168b
PeterPark1221 <137192346+PeterPark1221@users.noreply.github.com>
2024-02-24 19:01:30 +0900

    add searchView

---
+++ Netflix/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Netflix
+//
+//  Created by PeterPark on 2/24/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Netflix/Helpers/ActivityIndicator.swift
@@ -0,0 +1,8 @@
+//
+//  ActivityIndicator.swift
+//  Netflix
+//
+//  Created by PeterPark on 2/24/24.
+//
+
+import Foundation
