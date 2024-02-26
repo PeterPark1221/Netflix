af80feca6ff6afc975c828a2c0bafef8d8f97528
PeterPark1221 <137192346+PeterPark1221@users.noreply.github.com>
2024-02-26 15:57:53 +0900

    remake SearchView and add Detail

---
+++ Netflix/Helpers/DenerateMovies.swift
@@ -0,0 +1,8 @@
+//
+//  DenerateMovies.swift
+//  Netflix
+//
+//  Created by PeterPark on 2/26/24.
+//
+
+import Foundation
---
+++ Netflix/Search/SearchVM.swift
@@ -0,0 +1,8 @@
+//
+//  SearchVM.swift
+//  Netflix
+//
+//  Created by PeterPark on 2/26/24.
+//
+
+import Foundation
